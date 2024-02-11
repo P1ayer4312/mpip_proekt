42c1e9c50b1b67fd921974b4ddca6dd9baf6751c
P1ayer4312 <65920618+P1ayer4312@users.noreply.github.com>
2024-02-11 17:14:10 +0100

    init

---
+++ proekt_mpip_181190/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "proekt_mpip_181190"
+include(":app")
